f319d781ff5dc3d5ff5c447209a5217068d65786
sravani161 <sravanibalguri5@gmail.com>
2026-01-08 12:33:48 -0700

    made changes to projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,28 +3,28 @@ export const projects = [
     title: "Insight AI – Document-Aware Knowledge Assistant",
     description: "LLM assistant enabling PDF and document querying via natural language interaction and contextual summaries.",
     tech: ["Lang Chain | Hugging Face | Gemma | Streamlit"],
-    github: "#",
+    github: "https://github.com/sravani161/insightai",
     
   },
   {
     title: "CineGPT – AI-Powered Movie Recommendation Chatbot",
     description: "an intelligent movie-recommendation assistant combining TMDB data with LLM-based reasoning.",
     tech: ["Node.js | React | Hugging Face | LangChain | TMDB API"],
-    github: "#",
+    github: "https://github.com/sravani161/cinegpt",
   },
 
   {
     title: "Chronic Kidney Disease Analysis",
     description: "Developed a comprehensive analysis of chronic kidney disease data using Python. Performed data cleaning, pre-processing, and exploratory data analysis on a dataset containing patient information.",
     tech: ["Python (Programming Language) · Machine Learning · Data Analysis · Flask"],
-    github: "#",
+    github: "https://github.com/sravani161/CKD-Prediction",
   },
 
   {
     title: "Gaming Survey Dashboard",
     description: "Built an interactive Tableau dashboard with dynamic filters to enable insight exploration for non-technical stakeholder.",
     tech: ["Python (Pandas, Matplotlib, Seaborn) | Tableau"],
-    github: "#",
+    github: "https://github.com/sravani161/Data-Visualisation",
   },
 
 
